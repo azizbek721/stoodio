eda1f6e6bfc4d3cca89c8097eb254902be3decf4
azizbek721 <azizbekahmedov090@gmail.com>
2023-08-19 00:58:04 +0500

    The mobile version of the footer component is completed

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -3,46 +3,134 @@ import {FooterWrapper} from "./FooterWrapper";
 import footerLogo from "../../assets/logo.svg"
 import qrCode from "../../assets/qr-code.svg"
 import {Link} from "react-router-dom";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const Footer = () => {
+
+    const {width} = useWindowSize();
+
     return (
         <FooterWrapper>
             <div className="container mx-auto">
-                <div className="footer-over flex justify-between">
-                    <div className="footer-left flex flex-col justify-between">
-                        <Link to="/">
-                            <img src={footerLogo} alt="" className="footer-logo"/>
-                        </Link>
-                        <div className="footer-contact flex gap-x-5 m-y-4">
-                            <img src={qrCode} alt=""/>
-                            <div className="footer-contacts flex flex-col gap-y-5">
-                                <div className="phone-number">
-                                    <p>каждый день с 9 до 20</p>
-                                    <a href="tel:+998951952828">+99893 0000580</a>
+                {width > 850 ? (
+                    <div className="footer-over flex justify-between">
+                        {/* this is desktop version */}
+                        <div className="footer-left flex flex-col justify-between">
+                            <Link to="/">
+                                <img src={footerLogo} alt="" className="footer-logo"/>
+                            </Link>
+                            <div className="footer-contact flex gap-x-5 m-y-4">
+                                <img src={qrCode} alt=""/>
+                                <div className="footer-contacts flex flex-col gap-y-5">
+                                    <div className="phone-number">
+                                        <p>каждый день с 9 до 20</p>
+                                        <a href="tel:+998951952828">+99893 0000580</a>
+                                    </div>
+                                    <div className="instagram-account">
+                                        <p>instagram*</p>
+                                        <a href="https://www.instagram.com/stoodio.uz/?hl=ru"
+                                           target="blank">@stoodio.uz</a>
+                                    </div>
                                 </div>
-                                <div className="instagram-account">
-                                    <p>instagram*</p>
-                                    <a href="https://www.instagram.com/stoodio.uz/?hl=ru" target="blank">@stoodio.uz</a>
+                            </div>
+                            <p className="footer-left-text">
+                                Социальная сеть Instagram принадлежит компании Meta Platforms Inc., которая запрещена на
+                                территории РФ в связи с осуществлением экстремистской деятельности.
+                            </p>
+                        </div>
+                        <div className="footer-center flex flex-col gap-y-7">
+                            <Link to="/portfolio">портфолио</Link>
+                            <Link to="/team">stoodio.team</Link>
+                            <Link to="/jobs">вакансии</Link>
+                            <Link to="/contacts">контакты</Link>
+                        </div>
+                        <div className="footer-right flex flex-col gap-y-10">
+                            <Link to="/policy">политика <br/> конфиденциальности</Link>
+                            <span>Stoodio.uz является зарегистрированным товарным знаком №910417</span>
+                            <p>© 2006—2023 <br/>Архитектурное бюро Зиядиллы Наврузовой Stoodio.uz</p>
+                        </div>
+                    </div>
+                ) : width > 480 ? (
+                    <div className="footer-over flex gap-x-10">
+                        {/* this is tablet version */}
+                        <div className="footer-left flex flex-col justify-between">
+                            <Link to="/">
+                                <img src={footerLogo} alt="" className="footer-logo"/>
+                            </Link>
+                            <div className="footer-contact flex gap-x-5 m-y-4">
+                                <img src={qrCode} alt=""/>
+                                <div className="footer-contacts flex flex-col gap-y-2">
+                                    <div className="phone-number">
+                                        <p>каждый день с 9 до 20</p>
+                                        <a href="tel:+998951952828">+99893 0000580</a>
+                                    </div>
+                                    <div className="instagram-account">
+                                        <p>instagram*</p>
+                                        <a href="https://www.instagram.com/stoodio.uz/?hl=ru"
+                                           target="blank">@stoodio.uz</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="footer-left-text">
+                                Социальная сеть Instagram принадлежит компании Meta Platforms Inc., которая запрещена на
+                                территории РФ в связи с осуществлением экстремистской деятельности.
+                            </p>
+                            <Link to="/policy" className="policy-link">политика конфиденциальности</Link>
+                        </div>
+                        <div className="footer-center flex flex-col gap-y-7">
+                            <Link to="/portfolio">портфолио</Link>
+                            <Link to="/team">stoodio.team</Link>
+                            <Link to="/jobs">вакансии</Link>
+                            <Link to="/contacts">контакты</Link>
+
+                            <span>Stoodio.uz является зарегистрированным товарным знаком №910417</span>
+                            <p>© 2006—2023 <br/>Архитектурное бюро Зиядиллы Наврузовой Stoodio.uz</p>
+                        </div>
+
+                    </div>
+                ) : (
+                    <div className="footer-over">
+                        {/* this is mobile version */}
+                        <div className="footer-left flex flex-col justify-between">
+                            <Link to="/">
+                                <img src={footerLogo} alt="" className="footer-logo"/>
+                            </Link>
+                            <div className="footer-contact">
+                                <div className="footer-contacts flex flex-col gap-y-4">
+                                    <div className="phone-number">
+                                        <p>каждый день с 9 до 20</p>
+                                        <a href="tel:+998951952828">+99893 0000580</a>
+                                    </div>
+                                    <div className="instagram-account">
+                                        <p>instagram*</p>
+                                        <a href="https://www.instagram.com/stoodio.uz/?hl=ru"
+                                           target="blank">@stoodio.uz</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <p className="footer-left-text">
+                        <div className="footer-center flex flex-col gap-y-6">
+                            <Link to="/portfolio">портфолио</Link>
+                            <Link to="/team">stoodio.team</Link>
+                            <Link to="/jobs">вакансии</Link>
+                            <Link to="/contacts">контакты</Link>
+                        </div>
+                        <div className="instagram-account">
+                            <p>instagram*</p>
+                            <a href="https://www.instagram.com/stoodio.uz/?hl=ru"
+                               target="blank">@stoodio.uz</a>
+                        </div>
+                        <div className="footer-right flex flex-col gap-y-7">
+                            <Link to="/policy">политика конфиденциальности</Link>
+                            <span>Stoodio.uz является зарегистрированным товарным знаком №910417</span>
+                            <p>© 2006—2023 <br/>Архитектурное бюро Зиядиллы Наврузовой Stoodio.uz</p>
+                        </div>
+                        <p className="footer-mobile-bottom-text mt-5">
                             Социальная сеть Instagram принадлежит компании Meta Platforms Inc., которая запрещена на
                             территории РФ в связи с осуществлением экстремистской деятельности.
                         </p>
                     </div>
-                    <div className="footer-center flex flex-col gap-y-7">
-                        <Link to="/portfolio">портфолио</Link>
-                        <Link to="/team">stoodio.team</Link>
-                        <Link to="/jobs">вакансии</Link>
-                        <Link to="/contacts">контакты</Link>
-                    </div>
-                    <div className="footer-right flex flex-col gap-y-10">
-                        <Link to="/policy">политика <br/> конфиденциальности</Link>
-                        <span>Stoodio.uz является зарегистрированным товарным знаком №910417</span>
-                        <p>© 2006—2023 <br/>Архитектурное бюро Зиядиллы Наврузовой Stoodio.uz</p>
-                    </div>
-                </div>
+                )}
             </div>
         </FooterWrapper>
     );
--- src/containers/footer/FooterWrapper.js
+++ src/containers/footer/FooterWrapper.js
@@ -36,6 +36,9 @@ export const FooterWrapper = styled.div`
             font-size: 14px;
             line-height: 18px;
           }
+          a:hover {
+            color: #838383;
+          }
         }
       }
 
@@ -51,6 +54,9 @@ export const FooterWrapper = styled.div`
         color: white;
         line-height: 35px;
       }
+      a:hover {
+        color: #838383;
+      }
     }
     
     .footer-right {
@@ -59,6 +65,9 @@ export const FooterWrapper = styled.div`
         font-size: 14px;
         color: white;
         line-height: 18px;
+      }
+      a:hover {
+        color: #838383;
       }
       span {
         font-size: 14px;
@@ -72,6 +81,325 @@ export const FooterWrapper = styled.div`
         color: #838383;
         font-weight: 300;
         line-height: 18px;
+      }
+    }
+
+    .footer-mobile-bottom-text {
+      font-size: 14px;
+      line-height: 22px;
+      color: #838383;
+      width: 95%;
+    }
+  }
+
+  @media only screen and (max-width: 900px) {
+    padding: 90px 0 45px 0;
+
+    .footer-over {
+      .footer-left {
+        width: 70%;
+
+        .footer-logo {
+          width: 220px;
+          height: 74.18px;
+        }
+
+        .footer-contact {
+          margin: 30px 0 15px 0;
+          height: 89px;
+
+          img {
+            width: 89px;
+          }
+
+          .footer-contacts {
+
+            p {
+              color: #838383;
+              font-size: 14px;
+              padding: 0 !important;
+              line-height: 18px;
+            }
+
+            a {
+              color: white;
+              font-size: 14px;
+              line-height: 18px;
+            }
+            a:hover {
+              color: #838383;
+            }
+          }
+        }
+
+        .footer-left-text {
+          font-size: 12px;
+          color: #838383;
+        }
+      }
+
+      .footer-center {
+        a {
+          font-size: 24px;
+          color: white;
+          line-height: 35px;
+        }
+        a:hover {
+          color: #838383;
+        }
+      }
+
+      .footer-right {
+        width: 242px;
+        a {
+          font-size: 14px;
+          color: white;
+          line-height: 18px;
+        }
+        a:hover {
+          color: #838383;
+        }
+        span {
+          font-size: 14px;
+          color: #838383;
+          font-weight: 300;
+          opacity: 0.7;
+          line-height: 18px;
+        }
+        p {
+          font-size: 14px;
+          color: #838383;
+          font-weight: 300;
+          line-height: 18px;
+        }
+      }
+
+      .footer-mobile-bottom-text {
+        font-size: 14px;
+        line-height: 22px;
+        color: #838383;
+        width: 95%;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 700px) {
+    padding: 90px 0 45px 0;
+
+    .footer-over {
+      .footer-left {
+        width: 70%;
+
+        .footer-logo {
+          width: 220px;
+          height: 74.18px;
+        }
+
+        .footer-contact {
+          margin: 30px 0 15px 0;
+          height: 89px;
+
+          img {
+            width: 89px;
+          }
+
+          .footer-contacts {
+
+            p {
+              color: #838383;
+              font-size: 14px;
+              padding: 0 !important;
+              line-height: 18px;
+            }
+
+            a {
+              color: white;
+              font-size: 14px;
+              line-height: 18px;
+            }
+            a:hover {
+              color: #838383;
+            }
+          }
+        }
+
+        .footer-left-text {
+          font-size: 12px;
+          color: #838383;
+        }
+      }
+
+      .footer-center {
+        a {
+          font-size: 24px;
+          color: white;
+          line-height: 35px;
+        }
+        a:hover {
+          color: #838383;
+        }
+      }
+
+      .footer-right {
+        width: 242px;
+        a {
+          font-size: 14px;
+          color: white;
+          line-height: 18px;
+        }
+        a:hover {
+          color: #838383;
+        }
+        span {
+          font-size: 14px;
+          color: #838383;
+          font-weight: 300;
+          opacity: 0.7;
+          line-height: 18px;
+        }
+        p {
+          font-size: 14px;
+          color: #838383;
+          font-weight: 300;
+          line-height: 18px;
+        }
+      }
+
+      .footer-mobile-bottom-text {
+        font-size: 14px;
+        line-height: 22px;
+        color: #838383;
+        width: 95%;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    padding: 90px 0 45px 0;
+
+    .footer-over {
+      .footer-left {
+        width: 80%;
+
+        .footer-logo {
+          width: 220px;
+          height: 74.18px;
+        }
+
+        .footer-contact {
+          margin: 30px 0 15px 0;
+          height: 89px;
+
+          img {
+            width: 89px;
+          }
+
+          .footer-contacts {
+            p {
+              font-size: 14px;
+              line-height: 18px;
+            }
+            a {
+              font-size: 14px;
+              line-height: 18px;
+            }
+          }
+        }
+
+        .footer-left-text {
+          font-size: 12px;
+          color: #838383;
+        }
+      }
+
+      .footer-center {
+        a {
+          font-size: 24px;
+          color: white;
+          line-height: 35px;
+        }
+        a:hover {
+          color: #838383;
+        }
+      }
+
+      .footer-right {
+        width: 242px;
+        a {
+          font-size: 14px;
+          color: white;
+          line-height: 18px;
+        }
+        span {
+          font-size: 14px;
+          color: #838383;
+          font-weight: 300;
+          opacity: 0.7;
+          line-height: 18px;
+        }
+        p {
+          font-size: 14px;
+          color: #838383;
+          font-weight: 300;
+          line-height: 18px;
+        }
+      }
+
+      .footer-mobile-bottom-text {
+        font-size: 14px;
+        line-height: 22px;
+        color: #838383;
+        width: 95%;
+      }
+    }
+  }
+  
+  @media only screen and (max-width: 500px) {
+    padding: 40px 0;
+
+    .footer-over {
+      .footer-left {
+        width: 100%;
+
+        .footer-logo {
+          width: 180px;
+          height: 60.69px;
+        }
+        .footer-contact {
+          margin: 50px 0;
+          height: auto;
+
+          img {
+            width: 99px;
+          }
+
+          .footer-contacts {
+            p {
+              font-size: 14px;
+              line-height: 18px;
+            }
+
+            a {
+              font-size: 26px;
+              line-height: 32px;
+            }
+          }
+        }
+      }
+
+      .footer-center {
+        a {
+          font-size: 22px;
+          line-height: 26px;
+        }
+      }
+
+      .footer-right {
+        width: 95%;
+        a, span, p {
+          font-size: 14px;
+          line-height: 22px;
+        }
       }
     }
   }
