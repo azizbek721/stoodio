566af7412faaa0f531359aab02fca381c48f154b
azizbek721 <azizbekahmedov090@gmail.com>
2023-10-14 21:53:22 +0500

    Founders image changed and finished with adaptive

--- src/components/main/founder/Founder.js
+++ src/components/main/founder/Founder.js
@@ -11,25 +11,23 @@ const Founder = () => {
                 <div className="founder-div flex justify-around">
                     <div className="founder">
                         <img src={founder} alt=""/>
-                        <div className="founder-info flex flex-col gap-y-4">
-                            <h3 className="name">Зиёдилла <br/> Наврузов</h3>
-                            <p className="job">основатель <br/> бюро</p>
-                        </div>
                     </div>
                     <div className="founder-text flex flex-col justify-center items-start content-center">
-                        <Icon icon="ep:d-arrow-left" color="white" width="52" height="52" fontSize="100px" className=""/>
+                        <Icon icon="ep:d-arrow-left" color="white" width="52" height="52" fontSize="100px"
+                              className=""/>
                         <h5>
                             Мы создаем выразительные и функциональные пространства, руководствуясь образом жизни
                             современного человека, бюджетом заказчика и собственным взглядом на архитектуру и дизайн.
                         </h5>
                         <Icon icon="ep:d-arrow-right" color="white" width="52" height="52" className="right-icon"/>
+                        <p className="founders-title mt-8">основатели бюро</p>
+                        <div className="founders flex justify-between gap-x-4">
+                            <h3 className="name">Зиёдилла Наврузов</h3>
+                            <h3 className="name">Рахматулло Наврузов</h3>
+                        </div>
                     </div>
                     <div className="second-founder">
                         <img src={secondFounder} alt=""/>
-                        <div className="second-founder-info flex flex-col gap-y-4">
-                            <h3 className="name">Рахматулло <br/> Наврузов</h3>
-                            <p className="job">основатель <br/> бюро</p>
-                        </div>
                     </div>
                 </div>
             </div>
--- src/containers/modals/ResumeModal.js
+++ src/containers/modals/ResumeModal.js
@@ -1,13 +1,13 @@
-import React, {useEffect} from 'react';
-import {Resume} from '../discuss/DiscussWrapper';
-import {useDispatch, useSelector} from 'react-redux';
-import {closeResumeModal} from '../../store/reducers/resumeReducer';
+import React, { useEffect, useState } from 'react';
+import { Resume } from '../discuss/DiscussWrapper';
+import { useDispatch, useSelector } from 'react-redux';
+import { closeResumeModal } from '../../store/reducers/resumeReducer';
 import axios from 'axios';
 
-
 const ResumeModal = () => {
-    const {resume} = useSelector((state) => state.ResumeReducer);
+    const { resume } = useSelector((state) => state.ResumeReducer);
     const dispatch = useDispatch();
+    const [isSending, setIsSending] = useState(false);
 
     const closing = () => {
         dispatch(closeResumeModal());
@@ -22,59 +22,59 @@ const ResumeModal = () => {
     useEffect(() => {
         if (resume) {
             document.body.style.overflow = 'hidden';
-            document.addEventListener('keydown', handleKeyDown); // Добавляем обработчик события
+            document.addEventListener('keydown', handleKeyDown);
         } else {
             document.body.style.overflow = 'auto';
-            document.removeEventListener('keydown', handleKeyDown); // Удаляем обработчик события
+            document.removeEventListener('keydown', handleKeyDown);
         }
 
         return () => {
-            document.removeEventListener('keydown', handleKeyDown); // Очищаем обработчик события при размонтировании
+            document.removeEventListener('keydown', handleKeyDown);
         };
     }, [resume]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const formData = new FormData(e.target);
+        console.log('Submit button clicked!');
 
         try {
             // Замените 'YOUR_BOT_TOKEN' на токен вашего Telegram бота
             const botToken = '6596282708:AAE_Rj8yL_bQWkrbdFut9Cr5PC7ufF73YnU';
-
             // Замените 'YOUR_CHAT_ID' на ваш Chat ID в Telegram
             const chatId = '-4046818608';
 
-            const message = `
-        *Имя:* ${formData.get('Имя')}
-        *Телефон:* ${formData.get('Телефон')}
-        *E-mail:* ${formData.get('E-mail')}
-        *Город:* ${formData.get('Город')}
-        *Должность:* ${formData.get('Должность')}
-        *Портфолио:* ${formData.get('Портфолио') || 'не указано'}
-      `;
-
+            const formData = new FormData();
             formData.append('chat_id', chatId);
-            formData.append('document', formData.get('Резюме'));
+            formData.append('caption', `
+      Имя: ${e.target['Имя'].value}
+Телефон: ${e.target['Телефон'].value}
+E-mail: ${e.target['E-mail'].value}
+Город: ${e.target['Город'].value}
+Должность: ${e.target['Должность'].value}
+Портфолио: ${e.target['Портфолио'].value || 'не указано'}
+    `);
+            formData.append('document', e.target.Резюме.files[0]);
 
-            const botApiUrl = `https://api.telegram.org/bot${botToken}/sendDocument`;
-            await axios.post(botApiUrl, formData, {
+            await axios.post(`https://api.telegram.org/bot${botToken}/sendDocument`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
             });
 
-
             e.target.reset();
             closing();
         } catch (error) {
-            console.error('Ошибка отправки сообщения:', error);
+            console.error('Error sending the resume:', error);
+            console.log('Error response:', error.response);
+        } finally {
+            setIsSending(false); // Сбрасываем состояние отправки обратно в false
         }
     };
 
     return (
         <div>
-            <div className="overlay" style={{display: resume ? 'block' : 'none'}} onClick={closing}></div>
+            <div className="overlay" style={{ display: resume ? 'block' : 'none' }} onClick={closing}></div>
             <Resume resume={resume}>
                 <div className="modal-container mx-auto">
                     <div className="modal-top flex items-start">
@@ -84,19 +84,20 @@ const ResumeModal = () => {
                         <button className="modal-close-btn" onClick={closing}>X</button>
                     </div>
                 </div>
-                <hr/>
+                <hr />
                 <div className="modal-container mx-auto">
                     <form onSubmit={handleSubmit} className="modal-form flex flex-col gap-y-4">
-                        <input type="text" name="Имя" placeholder="Имя" required/>
-                        <input type="number" name="Телефон" placeholder="Телефон" required/>
-                        <input type="email" name="E-mail" placeholder="E-mail" required/>
-                        <input type="text" name="Город" placeholder="Город" required/>
-                        <input type="text" name="Должность" placeholder="Какая должность вам интересна" required/>
+                        <input type="text" name="Имя" placeholder="Имя" required />
+                        <input type="number" name="Телефон" placeholder="Телефон" required />
+                        <input type="email" name="E-mail" placeholder="E-mail" required />
+                        <input type="text" name="Город" placeholder="Город" required />
+                        <input type="text" name="Должность" placeholder="Какая должность вам интересна" required />
                         <label htmlFor="Резюме">Ваше резюме</label>
-                        <input type="file" name="Резюме" accept=".docx, .doc, .pdf" multiple
-                               placeholder="+ Выбрать файл"/>
-                        <input type="text" name="Портфолио" placeholder="Ссылка на ваше портфолио"/>
-                        <button className="modal-send-btn" type="submit">отправить</button>
+                        <input type="file" id="Резюме" accept=".docx, .doc, .pdf" multiple placeholder="+ Выбрать файл" />
+                        <input type="text" name="Портфолио" placeholder="Ссылка на ваше портфолио" />
+                        <button className="modal-send-btn" type="submit">
+                            {isSending ? 'Отправляется...' : 'Отправить'}
+                        </button>
                     </form>
                 </div>
             </Resume>
@@ -105,3 +106,8 @@ const ResumeModal = () => {
 };
 
 export default ResumeModal;
+
+
+// const botToken = '6596282708:AAE_Rj8yL_bQWkrbdFut9Cr5PC7ufF73YnU';
+
+// const chatId = '-4046818608';
